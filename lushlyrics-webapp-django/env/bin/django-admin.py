a4fc45e5f567a2b409f79fd9e0901fcea30aa4bf
Kaung Chit <kaungkaungpoem@gmail.com>
2023-10-09 22:14:14 +0900

    Remove duplicate files

---
+++ lushlyrics-webapp-django/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kaungchit/Desktop/ITStudies/pydjango/lushlyrics/dj-lushlyrics-insecure/lushlyrics-webapp-django/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
